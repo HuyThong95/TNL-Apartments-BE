f2707fe64587034265521f3ed4b88fc755b277e4
huythong95 <t.huythonght95@gmail.com>
2019-09-16 23:54:36 +0700

    implement jwt user details service

---
+++ src/main/java/com/codegym/tnlapartmentsbe/security/service/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,39 @@
+package com.codegym.tnlapartmentsbe.security.service;
+
+import com.codegym.tnlapartmentsbe.form.request.SignUpForm;
+import com.codegym.tnlapartmentsbe.model.User;
+import com.codegym.tnlapartmentsbe.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder bcryptEncoder;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+        return UserPrinciple.build(user);
+    }
+
+    public User save(SignUpForm user) {
+        User newUser = new User();
+        newUser.setUsername(user.getUsername());
+        newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+        return userRepository.save(newUser);
+    }
+}
