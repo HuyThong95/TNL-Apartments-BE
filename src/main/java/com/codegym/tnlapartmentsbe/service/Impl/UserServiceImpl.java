ab7a3600a30d54e9cd90c778c31d9d76bf99f877
huythong95 <t.huythonght95@gmail.com>
2019-09-16 21:56:52 +0700

    them annotation @Service

--- src/main/java/com/codegym/tnlapartmentsbe/service/Impl/UserServiceImpl.java
+++ src/main/java/com/codegym/tnlapartmentsbe/service/Impl/UserServiceImpl.java
@@ -4,9 +4,10 @@ import com.codegym.tnlapartmentsbe.model.User;
 import com.codegym.tnlapartmentsbe.repository.UserRepository;
 import com.codegym.tnlapartmentsbe.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
