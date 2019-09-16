dc93adde67b880946ce713474bd4de1cd9059bf1
huythong95 <t.huythonght95@gmail.com>
2019-09-16 19:44:28 +0700

    create User entity

---
+++ src/main/java/com/codegym/tnlapartmentsbe/model/User.java
@@ -0,0 +1,102 @@
+package com.codegym.tnlapartmentsbe.model;
+
+import org.hibernate.annotations.NaturalId;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table( name = "users",
+uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"username", "email"})
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min=3, max = 50)
+    private String name;
+
+    @NotBlank
+    @Size(min=3, max = 50)
+    private String username;
+
+    @NaturalId
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min=6, max = 100)
+    private String password;
+
+    private String avatar;
+
+    public User() {
+    }
+
+    public User(@NotBlank @Size(min = 3, max = 50) String name,
+                @NotBlank @Size(min = 3, max = 50) String username,
+                @NotBlank @Size(max = 50) @Email String email,
+                @NotBlank @Size(min = 6, max = 100) String password,
+                String avatar) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.avatar = avatar;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
