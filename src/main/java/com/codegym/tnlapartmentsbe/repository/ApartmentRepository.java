89928e2f36f2db0c86f71f5e55d6d3ca0719e5b7
huythong95 <t.huythonght95@gmail.com>
2019-09-16 19:46:43 +0700

    add annotation for Apartment repo

--- src/main/java/com/codegym/tnlapartmentsbe/repository/ApartmentRepository.java
+++ src/main/java/com/codegym/tnlapartmentsbe/repository/ApartmentRepository.java
@@ -2,6 +2,10 @@ package com.codegym.tnlapartmentsbe.repository;
 
 import com.codegym.tnlapartmentsbe.model.Apartment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
+@Repository
 public interface ApartmentRepository extends JpaRepository<Apartment, Long> {
 }
