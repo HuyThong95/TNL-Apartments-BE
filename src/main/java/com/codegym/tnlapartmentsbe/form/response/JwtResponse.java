ed675aa039b5d13d0dfc291b8e4df882c8fb883a
huythong95 <t.huythonght95@gmail.com>
2019-09-16 23:31:34 +0700

    add JWT response

---
+++ src/main/java/com/codegym/tnlapartmentsbe/form/response/JwtResponse.java
@@ -0,0 +1,47 @@
+package com.codegym.tnlapartmentsbe.form.response;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class JwtResponse {
+    private String token;
+    private String type = "Bearer";
+    private String username;
+    private Collection<? extends GrantedAuthority> roles;
+
+    public JwtResponse(String accessToken, String username, Collection<? extends GrantedAuthority> roles) {
+        this.token = accessToken;
+        this.username = username;
+        this.roles = roles;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Collection<? extends GrantedAuthority> getRoles() {
+        return roles;
+    }
+}
