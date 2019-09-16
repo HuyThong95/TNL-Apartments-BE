2e4bae05f70133f8b982add0b6311c837e36d4d3
huythong95 <t.huythonght95@gmail.com>
2019-09-16 22:08:41 +0700

    implement category service

--- src/main/java/com/codegym/tnlapartmentsbe/service/Impl/CategoryServiceImpl.java
+++ src/main/java/com/codegym/tnlapartmentsbe/service/Impl/CategoryServiceImpl.java
@@ -1,4 +1,45 @@
 package com.codegym.tnlapartmentsbe.service.Impl;
 
-public class CategoryServiceImpl {
+import com.codegym.tnlapartmentsbe.model.Category;
+import com.codegym.tnlapartmentsbe.repository.CategoryRepository;
+import com.codegym.tnlapartmentsbe.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category findById(Long id) {
+        return categoryRepository.findById(id).get();
+    }
+
+    @Override
+    public Category findByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    @Override
+    public void createCategory(Category category) {
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public void updateCategory(Category category) {
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
 }
