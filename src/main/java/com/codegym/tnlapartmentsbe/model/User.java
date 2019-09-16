31f0703405ad3dbf410cd4f7d61e650cd18db166
huythong95 <t.huythonght95@gmail.com>
2019-09-16 20:02:48 +0700

    connect table Role to table user with @ManyToMany

--- src/main/java/com/codegym/tnlapartmentsbe/model/User.java
+++ src/main/java/com/codegym/tnlapartmentsbe/model/User.java
@@ -6,6 +6,8 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table( name = "users",
@@ -36,6 +38,12 @@ public class User {
     private String password;
 
     private String avatar;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public User() {
     }
