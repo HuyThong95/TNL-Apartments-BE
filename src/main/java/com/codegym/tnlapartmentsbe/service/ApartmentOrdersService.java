2bf0264a31b0c07db8e7f5ee454431c4f8fb5f14
huythong95 <t.huythonght95@gmail.com>
2019-09-16 21:04:34 +0700

    create apartment orders service

---
+++ src/main/java/com/codegym/tnlapartmentsbe/service/ApartmentOrdersService.java
@@ -0,0 +1,28 @@
+package com.codegym.tnlapartmentsbe.service;
+
+import com.codegym.tnlapartmentsbe.model.ApartmentOrders;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+@Service
+public interface ApartmentOrdersService {
+    List<ApartmentOrders> findAll();
+    List<ApartmentOrders> findApartmentOrdersByTenantId( Long id );
+    List<ApartmentOrders> findApartmentOrdersByApartmentId(Long id);
+    List<Long> getOrderIdsByHouseId(Long id);
+    ApartmentOrders findById( Long id);
+    boolean existsApartmentOrdersByCheckinGreaterThanEqualAndCheckinLessThanEqualAndApartmentId(Date checkin, Date checkout, Long ApartmentId);
+    boolean existsApartmentOrdersByCheckoutGreaterThanEqualAndCheckoutLessThanEqualAndApartmentId(Date checkin, Date checkout, Long ApartmentId);
+    boolean existsApartmentOrdersByCheckinLessThanEqualAndCheckoutGreaterThanEqualAndApartmentId(Date checkin, Date checkout, Long ApartmentId);
+    boolean existsApartmentOrdersByCheckinGreaterThanEqualAndCheckoutLessThanEqualAndApartmentId(Date checkin, Date checkout, Long ApartmentId);
+    void createApartmentOrders(ApartmentOrders apartment);
+    void updateApartmentOrders(ApartmentOrders apartment);
+    void deleteApartmentOrders(Long id);
+    boolean existsApartmentStatusByStartDateGreaterThanEqualAndStartDateLessThanEqual(Date checkin, Date checkout, Long apartmentId);
+    boolean existsApartmentStatusByEndDateGreaterThanEqualAndEndDateLessThanEqual(Date checkin, Date checkout, Long apartmentId);
+    boolean existsApartmentStatusByStartDateLessThanEqualAndEndDateGreaterThanEqual(Date checkin, Date checkout, Long apartmentId);
+    boolean existsApartmentStatusByStartDateGreaterThanEqualAndEndDateLessThanEqual(Date checkin, Date checkout, Long apartmentId);
+
+
+}
